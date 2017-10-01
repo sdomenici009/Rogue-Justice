3acf91f64fcc35ab4bba2623e3d6a2eb4ba06d53
Stephen Domenici <sdomenici009@gmail.com>
2017-09-30 18:51:50 -0700

    Added a few scripts for a simple pathfinding implementation.

--- Rogue Justice/Assets/_Scripts/BoardManager.cs
+++ Rogue Justice/Assets/_Scripts/BoardManager.cs
@@ -3,8 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
-    void Start() {
 
+    [SerializeField]
+    private int width, height;
+
+    private BoardTile[,] boardTiles;
+
+    void Start() {
+        InitializeBoard();
+    }
+
+    private void InitializeBoard() { 
+        boardTiles = new BoardTile[width, height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                bool isWalkable = true;
+                if (j != 0 && j != height - 1)
+                    isWalkable = false;
+
+                boardTiles[i, j] = new BoardTile(i, j, isWalkable);
+            }
+        }
     }
 
     void Update() {
---
+++ Rogue Justice/Assets/_Scripts/BoardTile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct BoardTile {
+    public int x;
+    public int y;
+    public bool IsWalkable;
+
+    public BoardTile(int x, int y, bool isWalkable) {
+        this.x = x;
+        this.y = y;
+        this.IsWalkable = isWalkable;
+    }
+}
